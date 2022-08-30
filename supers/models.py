0a7d160c6f8a4396e056044add436f01512c9808
Mario Lugo <mlugo23699@gmail.com>
2022-08-29 18:32:47 -0700

    added super_types and supers to settings.py + created class models for apps

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,13 @@
+from ssl import ALERT_DESCRIPTION_NO_RENEGOTIATION
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class supers(models.Model):
+    name = models.CharField(max_length = 250)
+    alter_ego = models.CharField(max_length = 250)
+    primary_ability = models.CharField(max_length = 400)
+    secondary_ability = models.CharField(max_length = 400)
+    catch_phrase = models.CharField(max_length = 400)
+    super_type = models.ForeignKey()
