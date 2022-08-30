bf4b0ba445c298df564857285888f92e622e92a1
Mario Lugo <mlugo23699@gmail.com>
2022-08-29 22:43:03 -0700

    made modifications in supers' models.py and serializers.py to make migrations for querying on postman

--- supers/models.py
+++ supers/models.py
@@ -8,5 +8,5 @@ class Supers(models.Model):
     alter_ego = models.CharField(max_length = 250)
     primary_ability = models.CharField(max_length = 400)
     secondary_ability = models.CharField(max_length = 400)
-    catch_phrase = models.CharField(max_length = 400)
-    super_type = models.ForeignKey(SuperTypes, on_delete = models.CASCADE)
+    catchphrase = models.CharField(max_length = 400)
+    super_type_id = models.ForeignKey(SuperTypes, on_delete = models.CASCADE)
